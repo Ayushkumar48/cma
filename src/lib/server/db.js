ec583fa99b8c83d773c85a8063ffcfb1f2bc093c
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-11-17 01:01:54 +0530

    .

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -4,7 +4,7 @@ const MONGODB_URI = import.meta.env.MONGODB_URI;
 export async function connectDB() {
 	try {
 		if (mongoose.connection.readyState === 0) {
-			await mongoose.connect(MONGODB_URI);
+			await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 			console.log('Connected to MongoDB');
 		} else {
 			console.log('Already connected to MongoDB');
